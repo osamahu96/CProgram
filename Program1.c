bd378bcf907cf5999cd88b62cf09754545e6305f
= <=>
2017-07-10 21:40:44 +0500

    My First Program

---
+++ Program1.c
@@ -0,0 +1,12 @@
+// including the header files 
+#include<stdio.h>
+#include<conio.h>
+// defining the constant to be used on the 
+#define NAME "Osama Ahmed"
+// inintlizing the global variable
+int age = 20;
+// defining the main section of the type int the returns 0 
+int main() {
+	printf("My Name is %s and my age is %d \n", NAME , age);
+	return 0;
+}
